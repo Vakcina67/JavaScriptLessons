9f9816331e81ab98f13266552e39e957a8b1e793
Vakcina67 <89372590+Vakcina67@users.noreply.github.com>
2022-03-30 23:02:15 +0300

    for lesson 2

---
+++ ht5.js
@@ -0,0 +1,14 @@
+function funcPlus(a, b) {
+    return a + b;
+}
+function funcMinus(a, b) {
+    return a - b;
+}
+function funcMulti(a, b) {
+    return a * b;
+}
+function funcDiv(a, b) {
+    return a / b;
+}
+
+console.log(funcMinus(5, 7));
